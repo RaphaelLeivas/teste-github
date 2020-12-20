72d302dda99348a7b84c51624c9ef2dfdec877da
Raphael Leivas <raphaelhenrique2013@outlook.com>
2020-12-20 19:45:53 -0300

    commit 02 on register

--- components/Lista.js
+++ components/Lista.js
@@ -3,6 +3,7 @@ function Lista(props) {
         <div>
             <p>its me! hohoho</p>
             <h2>The title is AFTER - commit 1</h2>
+            <h3>The title is AFTER - commit 2</h3>
         </div>
     
     );

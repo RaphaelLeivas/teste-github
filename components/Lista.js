e7c0943f5cfc6f3fcd13c95d5f251bee8efa51eb
Raphael Leivas <raphaelhenrique2013@outlook.com>
2020-12-20 20:53:37 -0300

    alteracoes em Lista.js

--- components/Lista.js
+++ components/Lista.js
@@ -4,6 +4,7 @@ function Lista(props) {
             <p>its me! hohoho</p>
             <h2>The title is AFTER - commit 1</h2>
             <h3>The title is AFTER - commit 2</h3>
+            <li>mauna keaaa</li>
         </div>
     
     );

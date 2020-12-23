cbb200c8e18184bac756fb11bcc2d5ea7c581e6a
Raphael <raphaelhenrique2013@outlook.com>
2020-12-22 22:28:49 -0300

    mudancas gerais

--- teste.cpp
+++ teste.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
+
+#ifndef PI
+#define PI 3.14159
+
+#endif
 
 using namespace std;
 
+struct Dados {
+    int id;
+    string nome;
+};
+
 int main() {
     cout << "Hello World" << endl;
+
+    struct Dados pessoa;
+    struct Dados* database = &pessoa;
+    database->id = 100;
+    database->nome = "help me";
+
+    cout << database->nome << endl;
+    cout << "Funcionando." << endl;
 
     return 0;
 }

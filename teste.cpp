0e41aebc6fd9b23e586a28ae76dec6d02b09b7cd
Raphael <raphaelhenrique2013@outlook.com>
2020-12-29 19:30:15 -0300

    mudancas no codigo cpp

--- teste.cpp
+++ teste.cpp
@@ -13,16 +13,41 @@ struct Dados {
     string nome;
 };
 
-int main() {
-    cout << "Hello World" << endl;
+typedef enum {
+    NO_ERROR = 0,
+    SYSTEM_ERROR = 1,
+    FATAL_ERROR = 2
+} erro_t;
 
+struct Dados* fillData(struct Dados* data) {
+    cin >> data->nome;
+    cin >> data->id;
+
+    return data;
+}
+
+void errorHandler(erro_t index) {
+    if (index == SYSTEM_ERROR) cout << "SYSTEM_ERROR" << endl;
+    else if (index == FATAL_ERROR) cout << "FATAL_ERROR" << endl;
+
+    exit(1);
+
+    return;
+}
+
+int main() {
+    erro_t error;
     struct Dados pessoa;
     struct Dados* database = &pessoa;
-    database->id = 100;
-    database->nome = "help me";
+    database = fillData(database);
 
-    cout << database->nome << endl;
-    cout << "Funcionando." << endl;
+    if (database->nome == "joseavenca") {
+        error = FATAL_ERROR;
+        errorHandler(error);
+    }
+
+    cout << database->nome << endl << database->id << endl;
+
 
     return 0;
 }
